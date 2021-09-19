3122f3b49f5c32c1cff77110a9699737a4b3fdca
Syakhisk <mail.syakhisk@gmail.com>
2021-09-19 19:21:43 +0700

    Add LIKE query

--- src/routes/skor.js
+++ src/routes/skor.js
@@ -1,39 +1,67 @@
 const express = require("express");
 const router = express.Router();
+
 // const prisma = require('../lib/prisma')
+
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 router.get("/", async (req, res) => {
-  const { page = 1, count = 10, id_mapel, id_siswa } = req.query;
+  const { page = 1, count = 10, id_mapel, id_siswa, nama } = req.query;
 
-  const where = {}
+  const where = {};
 
-  if(id_mapel) where.id_mapel = Number(id_mapel)
-  if(id_siswa) where.id_siswa = Number(id_siswa)
+  if (id_mapel && id_mapel != 0) where.id_mapel = Number(id_mapel);
+  if (id_siswa) where.id_siswa = Number(id_siswa);
 
-  const siswaQuery = await prisma.result.findMany({
-    take: Number(count),
-    skip: Number((page - 1) * count),
-    where,
-    select: {
-      id: true,
-      skor: true,
-      siswa: { select: { nama: true } },
-      mapel: { select: { nama: true } },
-    },
-  });
+  let siswaQuery;
+  let serialized;
+  if (nama) {
+    const query = `SELECT Result.id as id, Siswa.nama as nama, Result.id_mapel as id_mapel, Result.skor as skor
+      FROM Result
+      INNER JOIN Siswa ON Result.id_siswa=Siswa.id
+      INNER JOIN Mata_Pelajaran ON Result.id_mapel=Mata_Pelajaran.id
+      WHERE Siswa.nama LIKE "%${nama}%"
+      ${id_mapel ? " AND id_mapel = " + id_mapel : ""}
+      LIMIT ${count}
+      OFFSET ${(page - 1) * 10}`;
 
-  const serialized = siswaQuery.map(
-    ({ id, siswa: { nama: namaSiswa }, mapel: { nama: namaMapel }, skor }) => ({
-      id,
-      nama: namaSiswa,
-      mapel: namaMapel,
-      skor,
-    })
-  );
+    siswaQuery = await prisma.$queryRawUnsafe(query);
+    serialized = siswaQuery;
+  } else {
+    siswaQuery = await prisma.result.findMany({
+      take: Number(count),
+      skip: Number((page - 1) * count),
+      where,
+      select: {
+        id: true,
+        skor: true,
+        siswa: { select: { nama: true } },
+        id_mapel: true,
+        // mapel: { select: { nama: true } },
+      },
+    });
 
-  res.send({ data: serialized });
+    serialized = siswaQuery.map(
+      ({ id, siswa: { nama: namaSiswa }, id_mapel, skor }) => ({
+        id,
+        nama: namaSiswa,
+        id_mapel,
+        skor,
+      })
+    );
+  }
+
+  // const serialized = siswaQuery.map(
+  //   ({ id, siswa: { nama: namaSiswa }, mapel: { nama: namaMapel }, skor }) => ({
+  //     id,
+  //     nama: namaSiswa,
+  //     // mapel: namaMapel,
+  //     skor,
+  //   })
+  // );
+
+  res.send(serialized);
 });
 
 router.get("/raw", async (req, res) => {
@@ -47,4 +75,5 @@ router.get("/raw", async (req, res) => {
 
   res.send({ result });
 });
+
 module.exports = router;
